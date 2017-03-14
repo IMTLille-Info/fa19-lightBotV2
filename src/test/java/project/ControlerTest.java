5ea969b0e12ee6f4ce4a1463d65d51c37f6e2fdb
hug007 <hugo.billiaert@gmail.com>
2017-03-14 17:47:39 +0100

    Mise ISO

--- src/test/java/project/ControlerTest.java
+++ src/test/java/project/ControlerTest.java
@@ -5,22 +5,35 @@ package project;
 
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+import java.util.List;
+
 import org.junit.Test;
 
 import junit.framework.*;
+import project.WindowGame.ControleurPerso;
 
 /**
- * @author TSXN4236
+ * @author ZMNF8866
  *
  */
+private static Properties prop;  // les propriétés
+private static FileInputStream propFile;  //le fichier de propriétés
 public class ControlerTest {
-    
-
-    private static final Object val = "1";
-
+	public List<ControleurPerso> listControl= new LinkedList<ControleurPerso>();
+	expectedSize = ControleurPerso.parseInt(prop.getProperty("taille"));
     @Test
-    public void test() {
-	assertEquals("1", val);
+    public void testadd() {
+    	assertEquals(expectedSize, listControl.size());
+    	listControl.add(ControleurPerso.haut);
+    	assertEquals(expectedSize+1, listControl.size());
+    	 assertThat(Month.valueOf("January"), is(notNullValue()));
+    	 List<String> e = mapperFactory.getMapperFacade().mapAsList(Arrays.asList(E.a, E.b, E.c), String.class);
+         
+         Assert.assertEquals(Arrays.asList("a", "b", "c"), e);
+    	
+
+	
 	//fail("Not yet implemented");
     }
 
